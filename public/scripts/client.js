8b53902fd2ac95155b4d5d52a98733c8f1870df7
nelsong44 <nelsong44@gmail.com>
2017-06-10 18:51:01 -0500

    completed base mode

---
+++ public/scripts/client.js
@@ -0,0 +1,48 @@
+console.log('JS linked');
+
+// creating new angular module named 'myApp'
+var myApp = angular.module('myApp', []);
+
+// adding a controller to the app
+myApp.controller('AngController', function($scope) {
+  console.log('NG linked'); //make sure angular is linked properly // NG will log in console once linked in HTMl
+  var vm = this; // creating two-way bind - connecting js and html
+  vm.userArray = [];
+  vm.addUser = function() {
+    console.log('in addInput()');
+    //get user input
+    //place into a new object
+    //push into array
+    var newUser = {
+      firstName: vm.firstNameIn,
+      lastName: vm.lastNameIn,
+      idNumber: vm.idNumberIn,
+      jobTitle: vm.jobTitleIn,
+      annualSalary: vm.annualSalaryIn
+    }; // end object
+    console.log(newUser);
+    vm.userArray.push(newUser);
+    console.log(vm.userArray);
+
+    // function to calculate average of salaries
+    vm.calcAverage = function() {
+      var av = 0;
+      var i = 0;
+      for (i = 0; i < vm.userArray.length; i++) {
+        av += vm.userArray[i].annualSalary;
+        console.log(vm.userArray[i].annualSalary);
+      }
+      console.log(av/12);
+      return '$' + av/12;
+    }; // end calcAverage
+
+    vm.calcAverage();
+
+    // clear input fields after info is stored in array and displayed on DOM
+    vm.firstNameIn='';
+    vm.lastNameIn='';
+    vm.idNumberIn='';
+    vm.jobTitleIn='';
+    vm.annualSalaryIn='';
+  }; // end addUser
+}); // end controller
